05578ae9da23ed39b5d888c7d0a7f8d6564b549f
jaabless <jefferyasamoahbless@gmail.com>
2025-09-19 11:43:33 +0000

    (test): Add CheckoutOverviewPage class for checkout overview and completion

--- src/main/java/com/swaglabs/pages/CheckoutOverviewPage.java
+++ src/main/java/com/swaglabs/pages/CheckoutOverviewPage.java
@@ -15,4 +15,17 @@ public class CheckoutOverviewPage extends BasePage {
     public String getTotal() {
         return $(".summary_total_label").getText();
     }
+
+    public boolean isFinishButtonVisible() {
+        return finishButton.isDisplayed();
+    }
+
+    public String getItemName() {
+        return $(".inventory_item_name").getText();
+    }
+
+    public String getItemTotal() {
+        return $(".summary_total_label").getText();
+    }
+
 }
