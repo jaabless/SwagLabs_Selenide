50c83d69621e3e884aa3e2be7b29c9bcf8144efd
jaabless <jefferyasamoahbless@gmail.com>
2025-09-20 00:43:13 +0000

    (test): Add before suite to run test in suite

--- src/test/java/com/swaglabs/tests/BaseTests.java
+++ src/test/java/com/swaglabs/tests/BaseTests.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.slf4j.Logger;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
@@ -15,32 +16,7 @@ import static com.codeborne.selenide.Selenide.open;
 public abstract class BaseTests {
     protected Logger logger = LoggerUtil.getLogger(this.getClass());
 
-//    @BeforeMethod
-//    public void setUp() {
-//        Configuration.browser = "chrome";
-//        Configuration.headless = true;
-//        Configuration.browserSize = "1920x1080";
-//        Configuration.timeout = 5000;
-////        Configuration.baseUrl = "https://www.saucedemo.com";
-//        open("https://www.saucedemo.com/");
-//
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--disable-dev-shm-usage");
-//        options.addArguments("--disable-gpu");
-//        options.addArguments("--remote-allow-origins=*");
-//
-//        Configuration.browserCapabilities = options;
-//
-////        Configuration.browser = System.getProperty("selenide.browser", "chrome");
-////        Configuration.headless = Boolean.parseBoolean(System.getProperty("selenide.headless", "true")); // Default headless
-////        Configuration.timeout = 10000; // 10s wait
-//        Configuration.reportsFolder = "target/selenide-reports"; // For screenshots
-////        Configuration.baseUrl = "https://www.saucedemo.com";
-//        logger.info("Starting test in {} browser, headless: {}", Configuration.browser, Configuration.headless);
-//    }
-
-    @BeforeMethod
+    @BeforeSuite(alwaysRun = true)
     public void setUp() {
         Configuration.browser = "chrome";
         Configuration.headless = true;
